a65bd7c750427b4926973341919607b618ab2da5
Yura Griga <grigach@gmail.com>
2016-05-31 23:24:30 +0300

    progress

--- rx-filter/js/bundle.js
+++ rx-filter/js/bundle.js
@@ -77,7 +77,7 @@ angular.module('app.rxFilter', []).config(function ($stateProvider) {
 "use strict";
 
 angular.module("ng").run(["$templateCache", function ($templateCache) {
-  $templateCache.put("app/rx-filter/rx-filter.html", "<md-content>\n    <p>duration, h: {{filter.duration.min}}-{{filter.duration.max}}</p>\n    <range-slider lower-value=\"filter.duration.min\" upper-value=\"filter.duration.max\" min-gap=\"1\" step=\"1\" min=\"1\"\n                  max=\"48\"></range-slider>\n    <p>price, $: {{filter.price.min}}-{{filter.price.max}}</p>\n    <range-slider lower-value=\"filter.price.min\" upper-value=\"filter.price.max\" min-gap=\"100\" step=\"100\" min=\"0\"\n                  max=\"10000\"></range-slider>\n\n    <md-input-container>\n        <label>Order By</label>\n        <md-select ng-model=\"filter.order\">\n            <md-option ng-repeat=\"item in vm.orders\" value=\"{{item.field}}\">\n                {{item.title}}\n            </md-option>\n        </md-select>\n    </md-input-container>\n\n\n    <md-list>\n        <md-subheader class=\"md-no-sticky\">Search results:</md-subheader>\n        <md-list-item class=\"md-3-line\" ng-repeat=\"item in vm.items\">\n\n            <div class=\"md-list-item-text\" layout=\"column\">\n                <h3>{{item.id}} ___ {{ item.title }} ___ {{ item.price | currency }} ___ {{item.duration}}</h3>\n\n            </div>\n        </md-list-item>\n        <md-divider></md-divider>\n    </md-list>\n</md-content>");
+  $templateCache.put("app/rx-filter/rx-filter.html", "<div id=\"rx-filter__page\" layout=\"column\" flex>\n    <p>duration, h: {{filter.duration.min}}-{{filter.duration.max}}</p>\n    <range-slider lower-value=\"filter.duration.min\" upper-value=\"filter.duration.max\" min-gap=\"1\" step=\"1\" min=\"1\"\n                  max=\"48\"></range-slider>\n    <p>price, $: {{filter.price.min}}-{{filter.price.max}}</p>\n    <range-slider lower-value=\"filter.price.min\" upper-value=\"filter.price.max\" min-gap=\"100\" step=\"100\" min=\"0\"\n                  max=\"10000\"></range-slider>\n\n    <md-input-container>\n        <label>Order By</label>\n        <md-select ng-model=\"filter.order\">\n            <md-option ng-repeat=\"item in vm.orders\" value=\"{{item.field}}\">\n                {{item.title}}\n            </md-option>\n        </md-select>\n    </md-input-container>\n\n\n    <md-content>\n    <md-list>\n        <md-subheader class=\"md-no-sticky\">Search results:</md-subheader>\n        <md-list-item class=\"md-3-line\" ng-repeat=\"item in vm.items\">\n\n            <div class=\"md-list-item-text\" layout=\"column\">\n                <h3>{{item.id}} ___ {{ item.title }} ___ {{ item.price | currency }} ___ {{item.duration}}h</h3>\n\n            </div>\n        </md-list-item>\n        <md-divider></md-divider>\n    </md-list>\n    </md-content>\n</div>");
   $templateCache.put("app/layout/views/footer.html", "<div class=\"footer-copyright\">\n&copy; M22\n</div>");
   $templateCache.put("app/layout/views/header.html", "<nav class=\"layout-row\">\n    <span class=\"flex\"></span>\n    <b>Rx Filter</b>\n</nav>");
   $templateCache.put("app/layout/views/layout.html", "<header data-ui-view=\"header\"></header>\n\n<section class=\"layout-row flex\" ng-cloak>\n\n    <div data-ui-view=\"content\" id=\"content\" class=\"layout-column flex\" data-autoscroll=\"false\"></div>\n\n</section>\n\n<footer data-ui-view=\"footer\"></footer>\n\n\n\n");
@@ -95,37 +95,37 @@ angular.module("ng").run(["$templateCache", function ($templateCache) {
 
 var items = [{
     id: 1,
-    duration: 3600 * 10,
+    duration: 10,
     title: 'Flight 1',
     price: 800
 }, {
     id: 2,
-    duration: 3600 * 5,
+    duration: 5,
     title: 'Flight 2',
     price: 600
 }, {
     id: 3,
-    duration: 3600 * 6,
+    duration: 6,
     title: 'Flight 3',
     price: 500
 }, {
     id: 4,
-    duration: 3600 * 11,
+    duration: 11,
     title: 'Flight 4',
     price: 1100
 }, {
     id: 5,
-    duration: 3600 * 12,
+    duration: 12,
     title: 'Flight 5',
     price: 1000
 }, {
     id: 6,
-    duration: 3600 * 22,
+    duration: 22,
     title: 'Flight 6',
     price: 2700
 }, {
     id: 7,
-    duration: 3600 * 23,
+    duration: 23,
     title: 'Flight 7',
     price: 2800
 }];
@@ -139,8 +139,9 @@ var Data = function Data() {
 
             var i = _.chain(items).filter(function (_i) {
                 return filter.price.min <= _i.price && _i.price <= filter.price.max;
+            }).filter(function (_i) {
+                return filter.duration.min <= _i.duration && _i.duration <= filter.duration.max;
             }).orderBy([filter.order]).value();
-            console.log(i);
             subject.onNext(i);
         },
         subscribe: function subscribe(cb) {
@@ -175,7 +176,7 @@ var RxFilterController = function RxFilterController($scope, Data) {
 
     $scope.filter = {
         duration: {
-            min: 12,
+            min: 6,
             max: 24
         },
         price: {
